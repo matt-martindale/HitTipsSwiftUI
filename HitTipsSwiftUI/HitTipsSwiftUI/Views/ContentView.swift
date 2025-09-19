0c95c578cc3c6b167b27de6b7ff4ec28d2006990
Matt Martindale <matt.martindale88@gmail.com>
2025-09-19 15:48:47 -0600

    Update tabview icons

--- HitTipsSwiftUI/HitTipsSwiftUI/Views/ContentView.swift
+++ HitTipsSwiftUI/HitTipsSwiftUI/Views/ContentView.swift
@@ -17,11 +17,11 @@ struct ContentView: View {
     
     var body: some View {
         TabView(selection: $currentTab) {
-            Tab("HitTips", systemImage: "house", value: TabIdentifier.home) {
+            Tab("HitTips", systemImage: "exclamationmark.square", value: TabIdentifier.home) {
                 HomeView()
                     .environmentObject(fireStoreManager)
             }
-            Tab("History", systemImage: "gear", value: TabIdentifier.history) {
+            Tab("History", systemImage: "newspaper", value: TabIdentifier.history) {
                 HistoryView()
             }
         }
