fea1380da042007de9e3435a7b01b4a7dee77c75
Matt Martindale <matt.martindale88@gmail.com>
2025-09-25 14:56:57 -0600

    Changed hype to uplifting, on backend only

--- HitTipsSwiftUI/functions/src/index.ts
+++ HitTipsSwiftUI/functions/src/index.ts
@@ -140,7 +140,7 @@ export const callExternalApiDev = onCall(
             content: [
               {
                 type: "input_text",
-                text: "You are a tipping calculator that will Roast the following restaurant tip like Santa Clause. Response should be 1-3 sentences.",
+                text: "You are a tipping calculator that will Roast the following restaurant tip like a news anchor. Response should be 1-3 sentences.",
               },
             ],
           },
